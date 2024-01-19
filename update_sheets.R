46e48738545ea166920278f875cd51a9e7d6ebeb
Jorge <77586212+Gronnd@users.noreply.github.com>
2024-01-19 18:18:35 +0100

    Corregir get_responses Cambiar get_participants por get_responses

--- update_sheets.R
+++ update_sheets.R
@@ -16,12 +16,18 @@ base64_to_df <- function(x) {
 
   return(read.csv(textConnection(raw_csv), stringsAsFactors = FALSE, sep = ";"))
 }
-get_participants <- function(iSurveyID, iStart, iLimit, bUnused, aAttributes) {
+
+get_responses <- function(iSurveyID, sDocumentType = "csv", sLanguageCode = NULL,
+                          sCompletionStatus = "complete", sHeadingType = "code",
+                          sResponseType = "long", ...) {
   # Put all the function's arguments in a list to then be passed to call_limer()
   params <- as.list(environment())
+  dots <- list(...)
+  if(length(dots) > 0) params <- append(params,dots)
+  # print(params) # uncomment to debug the params
 
-  results <- call_limer(method = "list_participants", params = params)
-  return(data.frame(results))
+  results <- call_limer(method = "export_responses", params = params)
+  return(base64_to_df(unlist(results)))
 }
 
 get_session_key <- function(username = getOption("lime_username"),
